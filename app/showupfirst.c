795bf5f3fc406dc9656366d9d46287487065c448
Danyakotik228 <45770906+Danyakotik228@users.noreply.github.com>
2019-05-21 14:07:26 +0300

    Update showupfirst.c

--- app/showupfirst.c
+++ app/showupfirst.c
@@ -41,12 +41,12 @@ static void showupfirstt(int index, char *contents, int cursor, void *data) {
   /* Капитализация символов после пробела */
   char line[MAXLINE];
   char output_line[MAXLINE];
-  strncpy(line, contents);
+  strncpy(line, contents, MAXLINE);
 
   if (cursor >= 0) {
     strncpy(output_line, line, cursor);
     output_line[cursor] = '|';
-    strncpy(output_line + cursor + 1, line + cursor);
+    strncpy(output_line + cursor + 1, line + cursor, MAXLINE);
     toupfirst(output_line);
     printf("%s", output_line);
     if (output_line[strlen(output_line) - 1] != '\n')
